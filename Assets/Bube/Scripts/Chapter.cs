03a4deaa5ae66f60a1b007c35d747721cd6939e7
Ketoia <48310919+Ketoia@users.noreply.github.com>
2022-12-11 06:15:43 +0100

    dialogs

--- Assets/Bube/Scripts/Chapter.cs
+++ Assets/Bube/Scripts/Chapter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Dialog", menuName = "Dialog", order = 1)]
+[CreateAssetMenu(fileName = "chapter", menuName = "chapter", order = 1)]
 public class Chapter : ScriptableObject
 {
     public List<Dialog> Dialogs;
