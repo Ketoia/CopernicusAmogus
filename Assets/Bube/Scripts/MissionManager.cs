b2c70fc33f83a7182db3545cd6e3112ac21991ac
Buba <48310919+Ketoia@users.noreply.github.com>
2022-12-11 08:42:38 +0100

    git gut

--- Assets/Bube/Scripts/MissionManager.cs
+++ Assets/Bube/Scripts/MissionManager.cs
@@ -10,6 +10,8 @@ public class MissionManager : MonoBehaviour
     public static MissionManager instance;
     public UnityEngine.UI.Text text;
     public DialogSystem dialogSystem;
+    public List<Vector4> AdditionalLayers;
+    private int counter = 0;
 
     private void Awake()
     {
@@ -22,16 +24,32 @@ public class MissionManager : MonoBehaviour
     {
         List<int> quests = new List<int>();
         text.text = "";
-        for (int i = 0; i < layers; i++)
-        {
-            int max = (i + 1);
-            int pizza = (int)Random.Range(1, 6 * max - 1);
-            //pizza = (int)Mathf.Lerp(0, max, currentLvl / maxLvls) + 1;
-            pizza = Mathf.Clamp(pizza, 1, 6 * max - 1);
-            text.text += "Set: " + pizza.ToString() + " atoms on " + (i + 1).ToString() + " layer \n";
 
-            quests.Add(pizza); 
+        if(counter < AdditionalLayers.Count)
+        {
+            quests.Add((int)AdditionalLayers[counter].x);
+            text.text += "Set: " + quests[0] + " atoms on " + (1).ToString() + " layer \n";
+            quests.Add((int)AdditionalLayers[counter].y);
+            text.text += "Set: " + quests[1] + " atoms on " + (2).ToString() + " layer \n";
+            quests.Add((int)AdditionalLayers[counter].z);
+            text.text += "Set: " + quests[2] + " atoms on " + (3).ToString() + " layer \n";
+            quests.Add((int)AdditionalLayers[counter].w);
+            text.text += "Set: " + quests[3] + " atoms on " + (4).ToString() + " layer \n";
         }
+        else
+        {
+            for (int i = 0; i < layers; i++)
+            {
+                int max = (i + 1);
+                int pizza = (int)Random.Range(1, 6 * max - 1);
+                //pizza = (int)Mathf.Lerp(0, max, currentLvl / maxLvls) + 1;
+                pizza = Mathf.Clamp(pizza, 1, 6 * max - 1);
+                text.text += "Set: " + pizza.ToString() + " atoms on " + (i + 1).ToString() + " layer \n";
+
+                quests.Add(pizza);
+            }
+        }
+        counter++;
 
         return quests;
     }
